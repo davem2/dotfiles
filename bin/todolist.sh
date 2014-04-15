89b263651c2051aa51e96e318acf85df3c12d6e6
David M <uberl33thax0r@hotmail.com>
2014-04-15 18:12:04 -0400

    remove filename extension for scripts in ~/bin

--- bin/todolist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for item in `todo.sh lsc|sed 's/@//g'`
-do
-        echo `echo $item|tr a-z A-Z`
-        todo.sh -p ls `echo @"$item"` |sed 's/@[^ ]*//g' |sed 's/^/  /'
-done
--- bin/urbanterror.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-DISPLAY=:1.1
-xinit -geometry =1680x1050+1280+0  /usr/bin/urbanterror $* -- :1
