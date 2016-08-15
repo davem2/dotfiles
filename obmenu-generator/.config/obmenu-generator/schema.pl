adba8f4ea92ff8742486d2c71b0f9f1bf03e5dcf
David Maranhao <uberl33thax0r@hotmail.com>
2016-08-15 15:50:21 -0400

    Lots of misc changes

--- obmenu-generator/.config/obmenu-generator/schema.pl
+++ obmenu-generator/.config/obmenu-generator/schema.pl
@@ -68,12 +68,12 @@ our $SCHEMA = [
     {sep => 'Applications'},
 
     #          NAME            LABEL                ICON
-    {cat => ['utility',     'Accessories', 'applications-utilities']},
+    {cat => ['utility',     'Utilities', 'applications-utilities']},
     {cat => ['development', 'Development', 'applications-development']},
     #{cat => ['education',   'Education',   'applications-science']},
     {cat => ['game',        'Games',       'applications-games']},
     {cat => ['graphics',    'Graphics',    'applications-graphics']},
-    {cat => ['audiovideo',  'Multimedia',  'applications-multimedia']},
+    {cat => ['audiovideo',  'Audio/Video', 'applications-multimedia']},
     {cat => ['network',     'Network',     'applications-internet']},
     {cat => ['office',      'Office',      'applications-office']},
     {cat => ['other',       'Other',       'applications-other']},
@@ -101,5 +101,5 @@ our $SCHEMA = [
     {item => ['xscreensaver-command -lock', 'Lock', 'lock']},
 
     # This options uses the default Openbox's action "Exit"
-    {exit => ['Exit', 'exit']},
+    {exit => ['Exit', 'exit',]},
 ]
